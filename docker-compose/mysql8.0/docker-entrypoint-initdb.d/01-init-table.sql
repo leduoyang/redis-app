f7ae3129b8d41dee34f7022bacbf284d72a509a5
leduoayng <leduoyang1995@gmail.com>
2024-12-05 23:51:42 -0800

    update: update table and mock data

--- docker-compose/mysql8.0/docker-entrypoint-initdb.d/01-init-table.sql
+++ docker-compose/mysql8.0/docker-entrypoint-initdb.d/01-init-table.sql
@@ -1,10 +1,35 @@
 USE base;
 
 CREATE TABLE task (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    task_id INT PRIMARY KEY AUTO_INCREMENT,
+    task_name VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    due_date DATETIME,
+    priority ENUM('LOW', 'MEDIUM', 'HIGH') NOT NULL,
+    status ENUM('PENDING', 'IN_PROGRESS', 'COMPLETED') DEFAULT 'PENDING',
+    assigned_to INT,
     description TEXT,
-    status ENUM('PENDING', 'IN_PROGRESS', 'DONE') DEFAULT 'PENDING',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    project_id INT NOT NULL,
+    INDEX idx_project_status (project_id, status),
+    INDEX idx_due_priority (due_date, priority),
+    FOREIGN KEY (assigned_to) REFERENCES user(user_id),
+    FOREIGN KEY (project_id) REFERENCES project(project_id)
+);
+
+
+CREATE TABLE project (
+    project_id INT PRIMARY KEY AUTO_INCREMENT,
+    project_name VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    description TEXT
+);
+
+CREATE TABLE user (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    role ENUM('LEAD', 'MANAGER', 'DEVELOPER', 'QA') NOT NULL
 );
